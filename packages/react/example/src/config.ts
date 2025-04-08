0c32c8e6240a9305f09f96edbd21113cdc6f4f5e
Kieran O'Neill <hello@kieranoneill.com>
2025-04-08 18:04:56 +0100

    build(react): move config in example to separate file

---
+++ packages/react/example/src/config.ts
@@ -0,0 +1,11 @@
+import type { CreateKatavaultParameters } from '@kibisis/katavault-core';
+
+const config: CreateKatavaultParameters = {
+  debug: true,
+  user: {
+    displayName: `Kieran O'Neill`,
+    username: `magnetartare`,
+  },
+};
+
+export default config;
--- packages/react/example/src/containers/App.tsx
+++ packages/react/example/src/containers/App.tsx
@@ -1,19 +1,13 @@
-import { CreateWalletParameters } from '@kibisis/katavault-core';
 import { KatavaultProvider } from '@kibisis/katavault-react';
 import { type FC } from 'react';
+
+// configs
+import config from '../config';
 
 // containers
 import Root from './Root';
 
 const App: FC = () => {
-  const config: CreateWalletParameters = {
-    debug: true,
-    user: {
-      displayName: `Kieran O'Neill`,
-      username: `magnetartare`,
-    },
-  };
-
   return (
     <KatavaultProvider config={config}>
       <Root />
