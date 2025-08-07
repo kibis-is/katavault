45a5057eac1bf04778582358ac5a901a442e269d
Kieran O'Neill <kieran@kibis.is>
2025-08-07 12:22:51 +0200

    refactor(core): removes example in favour of sandbox

--- packages/core/example/src/config.ts
+++
@@ -1,9 +0,0 @@
-import { AlgorandTestnet, VoiTestnet } from '@kibisis/chains';
-import type { CreateKatavaultParameters } from '@kibisis/katavault-core';
-
-const config: CreateKatavaultParameters = {
-  chains: [VoiTestnet, AlgorandTestnet],
-  debug: true,
-};
-
-export default config;
--- packages/core/example/src/handlers/index.ts
+++
@@ -1,3 +0,0 @@
-export { default as onAuthenticateViaUIButtonClick } from './onAuthenticateViaUIButtonClick';
-export { default as onClearButtonClick } from './onClearButtonClick';
-export { default as onOpenWalletButtonClick } from './onOpenVaultButtonClick';
