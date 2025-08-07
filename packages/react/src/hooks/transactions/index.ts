112866ed9d01a5d318ff0f1611602417879abe76
Kieran O'Neill <kieran@kibis.is>
2025-08-07 12:04:22 +0200

    chore: squash

--- packages/react/src/hooks/transactions/index.ts
+++ packages/react/src/hooks/transactions/index.ts
@@ -1,3 +1,3 @@
 export { default as useSendRawTransactions } from './useSendRawTransactions';
-export { default as useuseSignAndSendRawTransactionsSignRawTransactions } from './useSignAndSendRawTransactions';
+export { default as useSignAndSendRawTransactions } from './useSignAndSendRawTransactions';
 export { default as useSignRawTransactions } from './useSignRawTransactions';
